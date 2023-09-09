aaf42e97a46bd66f3660794cfbe334f3117df9df
varmanaa <vrajakul@gmail.com>
2023-09-10 01:37:21 +0200

    feat: add roles on join

--- src/events/member_add.rs
+++ src/events/member_add.rs
@@ -1,8 +1,14 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{cmp, collections::HashSet, sync::Arc};
 
-use twilight_model::gateway::payload::incoming::MemberAdd;
+use twilight_model::{
+    gateway::payload::incoming::MemberAdd,
+    id::{marker::RoleMarker, Id},
+};
 
-use crate::types::{context::Context, Result};
+use crate::{
+    types::{context::Context, Result},
+    utility::constants::FLUCTUATING_XP,
+};
 
 pub async fn handle_member_add(
     context: Arc<Context>,
@@ -10,11 +16,9 @@ pub async fn handle_member_add(
 ) -> Result<()> {
     let guild_id = payload.guild_id;
     let user_id = payload.user.id;
-    let (xp, last_message_timestamp) = context
-        .database
-        .get_member(guild_id, user_id)
-        .await?
-        .unwrap_or_default();
+    let Some(guild) = context.cache.get_guild(guild_id) else {
+        return Ok(());
+    };
     let avatar_url = if let Some(member_avatar) = payload.avatar {
         format!("https://cdn.discordapp.com/guilds/{guild_id}/users/{user_id}/avatars/{member_avatar}.png")
     } else if let Some(user_avatar) = payload.user.avatar {
@@ -28,6 +32,38 @@ pub async fn handle_member_add(
 
         format!("https://cdn.discordapp.com/embed/avatars/{index}.png")
     };
+    let (current_xp, last_message_timestamp) = context
+        .database
+        .get_member(guild_id, user_id)
+        .await?
+        .unwrap_or_default();
+    let current_level = FLUCTUATING_XP
+        .iter()
+        .position(|&level| current_xp.lt(&level.1))
+        .map_or(100, |position| FLUCTUATING_XP[cmp::max(position - 1, 0)].0);
+    let mut role_ids = HashSet::from_iter(payload.roles.clone());
+    let level_role_ids = guild
+        .levels
+        .read()
+        .to_owned()
+        .into_iter()
+        .filter_map(|(level, role_ids)| level.le(&current_level).then(|| role_ids))
+        .flatten()
+        .collect::<HashSet<Id<RoleMarker>>>();
+
+    role_ids.extend(level_role_ids);
+
+    context
+        .http
+        .update_guild_member(guild_id, user_id)
+        .roles(
+            &role_ids
+                .clone()
+                .into_iter()
+                .collect::<Vec<Id<RoleMarker>>>(),
+        )
+        .await?;
+
     context.cache.insert_member(
         avatar_url,
         payload.user.bot,
@@ -35,11 +71,11 @@ pub async fn handle_member_add(
         guild_id,
         None,
         last_message_timestamp,
-        HashSet::from_iter(payload.roles.clone()),
+        role_ids,
         user_id,
         payload.user.name.clone(),
         None,
-        xp,
+        current_xp,
     );
 
     Ok(())
--- src/events/member_chunk.rs
+++ src/events/member_chunk.rs
@@ -1,22 +1,27 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{cmp, collections::HashSet, sync::Arc};
 
-use twilight_model::gateway::payload::incoming::MemberChunk;
+use twilight_model::{
+    gateway::payload::incoming::MemberChunk,
+    id::{marker::RoleMarker, Id},
+};
 
-use crate::types::{context::Context, Result};
+use crate::{
+    types::{context::Context, Result},
+    utility::constants::FLUCTUATING_XP,
+};
 
 pub async fn handle_member_chunk(
     context: Arc<Context>,
     payload: MemberChunk,
 ) -> Result<()> {
     let guild_id = payload.guild_id;
+    let Some(guild) = context.cache.get_guild(guild_id) else {
+        return Ok(());
+    };
 
     for member in payload.members {
         let user_id = member.user.id;
-        let (xp, last_message_timestamp) = context
-            .database
-            .get_member(guild_id, user_id)
-            .await?
-            .unwrap_or_default();
+
         let avatar_url = if let Some(member_avatar) = member.avatar {
             format!("https://cdn.discordapp.com/guilds/{guild_id}/users/{user_id}/avatars/{member_avatar}.png")
         } else if let Some(user_avatar) = member.user.avatar {
@@ -31,6 +36,38 @@ pub async fn handle_member_chunk(
             format!("https://cdn.discordapp.com/embed/avatars/{index}.png")
         };
 
+        let (current_xp, last_message_timestamp) = context
+            .database
+            .get_member(guild_id, user_id)
+            .await?
+            .unwrap_or_default();
+        let current_level = FLUCTUATING_XP
+            .iter()
+            .position(|&level| current_xp.lt(&level.1))
+            .map_or(100, |position| FLUCTUATING_XP[cmp::max(position - 1, 0)].0);
+        let mut role_ids = HashSet::from_iter(member.roles.clone());
+        let level_role_ids = guild
+            .levels
+            .read()
+            .to_owned()
+            .into_iter()
+            .filter_map(|(level, role_ids)| level.le(&current_level).then(|| role_ids))
+            .flatten()
+            .collect::<HashSet<Id<RoleMarker>>>();
+
+        role_ids.extend(level_role_ids);
+
+        context
+            .http
+            .update_guild_member(guild_id, user_id)
+            .roles(
+                &role_ids
+                    .clone()
+                    .into_iter()
+                    .collect::<Vec<Id<RoleMarker>>>(),
+            )
+            .await?;
+
         context.cache.insert_member(
             avatar_url,
             member.user.bot,
@@ -38,11 +75,11 @@ pub async fn handle_member_chunk(
             guild_id,
             None,
             last_message_timestamp,
-            HashSet::from_iter(member.roles.clone()),
+            role_ids,
             user_id,
             member.user.name.clone(),
             None,
-            xp,
+            current_xp,
         );
     }
 
